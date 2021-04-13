41c9740d0a1a87768ffbdd8d626bdcb993740b9b
vinay <vinay@DESKTOP-HP8PO56>
2021-04-12 23:38:01 -0400

    Commiting project1 from teamlead

---
+++ Git-Project1/src/com/in28minutes/Demo1.java
@@ -0,0 +1,23 @@
+package com.in28minutes;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+	   
+		
+			
+			int i = 75;
+			
+			if(i<80) {
+				System.out.println("Good");
+			}else {
+			   System.out.println("None");
+			}
+	          }
+			
+			
+		}
+	
+
+
+
